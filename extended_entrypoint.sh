9f1722cdc13d993a228f7a44cc2ef8e000441b4b
Jakub Krzywda <jakub@elastisys.com>
2020-07-20 14:15:13 +0000

    Configure S3 as primary storage using envsubst

--- extended_entrypoint.sh
+++ extended_entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -eu
 
+echo "Configuring S3 as primary storage"
+envsubst '$S3_BUCKET $S3_KEY $S3_SECRET $S3_HOSTNAME $S3_REGION' < /var/www/html/config/s3.config.template > /var/www/html/config/s3.config.php
+echo "S3 storage configured"
+
 su -p www-data -s /bin/bash -c /gss_config.sh &
 /entrypoint.sh "$@"
