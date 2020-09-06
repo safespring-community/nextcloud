79416dd109e1765dfd889322599658406fb936f5
Jakub Krzywda <jakub@elastisys.com>
2020-09-06 17:09:25 +0200

    Add redeploy script

---
+++ redeploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eu
+
+echo "Redeploying Nextcloud Server"
+
+docker rm -f nextcloud_app_1 nextcloud_cron_1 nextcloud_db_1 nextcloud_redis_1 nextcloud_proxy_1
+docker volume prune -f
+
+echo "Storage type: "${STORAGE_TYPE}
+
+if [ ${STORAGE_TYPE} == 's3' ]
+then
+  source s3.env
+  s3cmd rm s3://${S3_BUCKET} --recursive --force
+fi
+
+docker build --no-cache -t jakubkrzywda/nextcloud:apache .
+docker-compose up -d
+
+echo "Nextcloud Server Redeployed"
