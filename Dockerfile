53ffacb68b3807c1fc07d3e4d2ca89489cca9fe7
Jakub Krzywda <jakub@elastisys.com>
2020-09-10 12:52:37 +0000

    Revert to the default Nextcloud S3 as primary storage config

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,6 @@
 ARG NEXTCLOUD_BASE_IMAGE=nextcloud:19.0-apache
 FROM ${NEXTCLOUD_BASE_IMAGE}
 
-# install envsubst required for S3 configuration
-RUN apt-get update && apt-get install -y gettext-base
-COPY --chown=www-data:www-data ./s3.config.template /var/www/html/config/
-
 COPY --chown=www-data:www-data ./gss_config.sh /
 COPY --chown=www-data:www-data ./mail_config.sh /
 COPY --chown=root:root ./extended_entrypoint.sh /
--- extended_entrypoint.sh
+++ extended_entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -eu
 
-echo "Storage type: "${STORAGE_TYPE}
-
-if [ ${STORAGE_TYPE} == 's3' ]
-then
-  echo "Configuring S3 as primary storage"
-  envsubst '$S3_BUCKET $S3_KEY $S3_SECRET $S3_HOSTNAME $S3_REGION' < /var/www/html/config/s3.config.template > /var/www/html/config/s3.config.php
-  echo "S3 storage configured"
-fi
-
 su -p www-data -s /bin/bash -c /gss_config.sh &
 su -p www-data -s /bin/bash -c /mail_config.sh &
 /entrypoint.sh "$@"
