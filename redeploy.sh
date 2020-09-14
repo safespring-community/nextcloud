9ea5269a545f4f3625663a8943d918639a5062e0
Jakub Krzywda <jakub@elastisys.com>
2020-09-14 12:17:30 +0000

    Make erasing S3 bucket conditional

--- redeploy.sh
+++ redeploy.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 set -eu
 
+POSITIONAL=()
+ERASE_S3='0'
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -e|--erase)
+    ERASE_S3='1'
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
 echo "Redeploying Nextcloud Server"
 
 docker rm -f nextcloud_app_1 nextcloud_cron_1 nextcloud_db_1 nextcloud_redis_1 nextcloud_proxy_1 || true
 docker volume prune -f
 
-source nextcloud.env
-echo "Storage type: "${STORAGE_TYPE}
-
-if [ ${STORAGE_TYPE} == 's3' ]
+if [ ${ERASE_S3} == '1' ]
 then
   source s3.env
-  s3cmd rm s3://${S3_BUCKET} --recursive --force
+  echo "Erasing S3 bucket [${OBJECTSTORE_S3_BUCKET}]"
+  s3cmd rm s3://${OBJECTSTORE_S3_BUCKET} --recursive --force
 fi
 
+source nextcloud.env
+export NEXTCLOUD_VERSION
 docker-compose build --no-cache
 docker-compose up -d
 
